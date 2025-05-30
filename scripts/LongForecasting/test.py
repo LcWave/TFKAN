9b1730641fa6a01dc3caeba9becf58004b14ded8
LcWave <86061264+LcWave@users.noreply.github.com>
2025-05-30 18:47:21 +0800

    Delete scripts/LongForecasting directory

--- scripts/LongForecasting/test.py
+++
@@ -1,9 +0,0 @@
-import optuna
-
-def objective(trial):
-    return trial.suggest_float("x", -5, 5)**2
-
-study = optuna.create_study()
-study.optimize(objective, n_trials=100)
-
-print("Best trial:", study.best_trial.params)
